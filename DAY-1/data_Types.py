7f13dd8b77287d41c0905f38ccbf87f4622dff7e
WITWICKY1982 <roadhruv43@gmail.com>
2026-06-28 16:18:20 +0530

    Day 1: Variables, Print Statements and Data Types

---
+++ DAY-1/data_Types.py
@@ -0,0 +1,18 @@
+#NUMERIC = Integer,Float,complex
+#STRINGS
+#BOOLEAN
+#COLLECTION-LIST,TUPLE,DICTITONARY,SET
+
+
+#NUMERIC
+ 
+#INTEGER
+a = 10
+b = 11
+print(a)
+print(type(a))
+
+#FLOAT
+c = 10.14
+print(c)
+print(type(c))

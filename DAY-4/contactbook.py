592b63ba5d50be8ebe8cc88f50c47276f57feead
WITWICKY1982 <roadhruv43@gmail.com>
2026-07-01 19:58:28 +0530

    DAY 4:EDITED PROJECT ADDED

--- DAY-4/contactbook.py
+++ DAY-4/contactbook.py
@@ -1,30 +1,32 @@
 contact = {}
-def add_contact(name,number):
+def add_contact():
+    name = input("ENTER NAME")
+    number = input("ENTER NUMBER1")
     contact[name] = number
     print("CONTACT SAVED")
-add_contact("DHRUV",9259588471)
-add_contact("pranav",893478724)
-add_contact("papa",8800960960)
 
 def view_contacts():
     for key,value in contact.items():
         print(f"{key}=={value}")
 
-def search_contact(name):
+def search_contact():
+    name = input("ENTER NO TO SEARCH1")
 
     print(contact.get(name))
 
-search_contact("DHRUV")
+
 
 def menu():
-    print("ENTER 1 FOR ADDING CONTACTS","\n","ENTER 2 FOR VIEW CONTACTS","\n","ENTER 3 FOR SEARCH CONTACTS")
+    print("ENTER 1 FOR ADDING CONTACTS","\n","ENTER 2 FOR VIEW CONTACTS","\n","ENTER 3 FOR SEARCH CONTACTS","\n","5 FOR EXIT")
     w =int(input("ENTER HERE"))
     if(w==1):
-        add_contact(name,number)
+        add_contact()
     elif(w==2):
         view_contacts()
     elif(w==3):
-        search_contact(name1)
+        search_contact()
+    elif(w==5):
+        print("EXUTED")
     
 menu()
 

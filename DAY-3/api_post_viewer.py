719fa39135e040fe5431564b57aa10bb6cb61cbc
WITWICKY1982 <roadhruv43@gmail.com>
2026-07-01 18:01:15 +0530

    DAY 4:function,lambda function,list,contact book,factorial

--- DAY-3/api_post_viewer.py
+++ DAY-3/api_post_viewer.py
@@ -4,6 +4,9 @@ post_id = int(input("ENTER POST ID"))
 response = requests.get(f"https://jsonplaceholder.typicode.com/posts/{post_id}")
 
 print(response.status_code)
-print(response.json())
+data = response.json()
+print("User Id : " , data.get("userId"))
+print(" ID : ",data.get("id"))
+print("TITLE:",data.get("title"))
 
 
